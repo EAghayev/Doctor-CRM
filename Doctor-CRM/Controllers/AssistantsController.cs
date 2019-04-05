bad6e7092cab5f7e2dac2755a832f30df2ef993b
mrgazanfarli <mr.gazanfarli@gmail.com>
2019-04-06 01:12:15 +0400

    First

---
+++ Doctor-CRM/Controllers/AssistantsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Doctor_CRM.Controllers
+{
+    public class AssistantsController : Controller
+    {
+        // GET: Assistants
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
